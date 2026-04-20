062f42caebc47a428933cb282c89e7f40664c303
Bertanokt <bertan.okt34@gmail.com>
2026-04-20 14:34:21 +0300

    Update main.py

--- Email/main.py
+++ Email/main.py
@@ -1,17 +1,22 @@
 import requests
 from send_email import send_email
 from datetime import datetime
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 today = datetime.now().strftime("%Y-%m-%d")
 
 topic = ("Turkey")
-API_KEY= "635a70c8b1e1402daa3116a4aeecdd89"
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+NEWS_API_KEY= os.getenv("NEWS_API_KEY")
 
 url = ("https://newsapi.org/v2/everything?" 
-      f"qintitle={topic}&"
+      f"q={topic}&"
        f"from={today}"
       "to=2026-04-15&sortBy=popularity&"
-      "apiKey=635a70c8b1e1402daa3116a4aeecdd89&"
+      f"apiKey= {NEWS_API_KEY}"
       "language=en")
 
 #Make request
@@ -19,15 +24,22 @@ response = requests.get(url)
 
 # Get a dictionary with a data
 content = response.json()
+articles = content["articles"]
 
-#Access the article titles and description
-body = "Subject: Today's news\n\n"
-for article in content["articles"][:20]:
-    if article["title"] is not None:
-        body += article["title"] + "\n"
-        body += (article["description"] or "") + "\n"
-        body += article["url"] + "\n\n"
+#AI summarizing the news
+model = init_chat_model(model="gemini-3-flash-preview",
+                        model_provider="google-genai",
+                        api_key = GOOGLE_API_KEY)
+prompt = f"""
+You are a news summarizer.
+Write a two short paragraph analyzing those news.
+Here are the news articles: {articles} """
 
+response = model.invoke(prompt)
+response_str = response.content[0]["text"]
+print(response_str)
+
+body = "Subject: News Summary\n\n" + response_str + "\n\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
